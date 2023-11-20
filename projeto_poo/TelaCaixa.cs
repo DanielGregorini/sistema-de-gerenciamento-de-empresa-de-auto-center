bea9b7dbd8a1bf60bae31f0894584bd133d6c780
Daniel Mahl Gregorini <danielgregorini@alunos.utfpr.edu.br>
2023-11-20 09:53:57 -0300

    tela caixa incio

---
+++ projeto_poo/TelaCaixa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoCenter
+{
+    public partial class TelaCaixa : Form
+    {
+        public TelaCaixa()
+        {
+            InitializeComponent();
+        }
+    }
+}
